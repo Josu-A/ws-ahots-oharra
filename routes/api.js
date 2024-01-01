67caddc7a10e513e01387908d94fc843206b5522
Josu Aguinaga Bengoetxea <32521451+Josu-A@users.noreply.github.com>
2024-01-01 23:40:23 +0000

    implement upload endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const apiController = require('../controllers/apiController');
 const multer = require('multer');
+const mongojs = require('mongojs');
+const db = mongojs('grabaketak', ['users']);
 
 const uploadFolder = 'recordings/';
 
@@ -32,8 +33,47 @@ const upload = multer({
     }
 }).single('recording');
 
+const handleList = async (id) => {
+    let filesFromUser = [];
+    db.users.find({ "name" : id },
+        { "filename" : 1, "date" : 1, "_id" : 0 },
+        { "$sort" : { "date" : -1 }, "limit" : 5 },
+        (error, userFiles) => {
+            if (error) {
+                console.error(error);
+            }
+            else {
+                filesFromUser = userFiles;
+            }
+        });
+    return { "files" : filesFromUser };
+};
 router.get('/list/:id', (req, res) => {
-    res.json(apiController.handleList(req.params.id));
+    return res.status(200).json(handleList(req.params.id));
+});
+
+router.post('/upload/:name', (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            return res.status(400).json({ "error" : err });
+        }
+    
+        const newAudioEntry = {
+            "name" : req.params.name,
+            "filename" : req.file.filename,
+            "date" : Date.now(),
+            "accessed" : Date.now()
+        };
+    
+        db.users.insert(newAudioEntry, (error, audioEntry) => {
+            if (error) {
+                console.error(error);
+                return res.status(404).json({ "error" : "Audio berriaren metadatua ez da sartu data basean." });
+            }
+            console.log(audioEntry);
+            return res.status(201).json(handleList(req.params.name));
+        });
+    });
 });
 
 module.exports = router;
