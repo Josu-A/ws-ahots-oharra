94639fd1f137dd166d937b6108536ea92fb5c8d1
Josu-A <aginagajosu@gmail.com>
2024-01-07 14:03:15 +0100

    add github auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,27 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/google', passport.authenticate('google',{
-        "scope" : ["profile", "email"]
-    }
-));
+    "scope" : ["profile", "email"]
+}));
 
 router.get('/google/callback', passport.authenticate('google', { "failureRedirect" : "/login" }),
     (req, res) => {
         req.session.userid = req.user._id.toString();
         req.session.username = req.user.username;
-        res.redirect('/');
+        res.status(200).redirect('/');
     }
  );
+
+router.get('/github', passport.authenticate('github', {
+        "scope" : ["user:email"]
+}));
+
+router.get('/github/callback', passport.authenticate('github', { "failureRedirect" : "/login" }),
+    (req, res) => {
+        req.session.userid = req.user._id.toString();
+        req.session.username = req.user.username;
+        res.status(200).redirect('/');
+    }
+);
 
 module.exports = router;
--- utils/passport.js
+++ utils/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const GitHubStrategy = require('passport-github2');
 require('dotenv').config();
 const mongojs = require('mongojs');
 const db = mongojs('grabaketak', ['userdata']);
@@ -23,14 +24,44 @@ passport.use(new GoogleStrategy({
                     "username" : profile.displayName,
                     "email" : profile.emails[0].value,
                     "googleId" : profile.id
-                }
+                };
                 db.userdata.insert(newUser, (error, user) => {
                     if (error) {
                         console.error(error);
                     }
                     done(error, user);
-                })
+                });
             }
+        });
+    }
+));
+
+passport.use(new GitHubStrategy({
+        "clientID" : process.env.GITHUB_CLIENT_ID,
+        "clientSecret" : process.env.GITHUB_CLIENT_SECRET,
+        "callbackURL" : "/auth/github/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+        db.userdata.findOne({ "githubId" : profile.id }, (error, user) => {
+            if (error) {
+                console.error(error);
+                return done(error, undefined);
+            }
+            if (user) {
+                done(null, user);
+            }
+            else {
+                const newUser = {
+                    "username" : profile.displayName,
+                    "email" : profile.emails[0].value,
+                    "githubId" : profile.id
+                };
+                db.userdata.insert(newUser, (error, user) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                    done(error, user);
+                });
         });
     }
 ));
