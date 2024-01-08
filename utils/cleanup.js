8cefcc71e07f326d6bc06d1b7931c92ef64595a1
Josu-A <aginagajosu@gmail.com>
2024-01-08 01:07:16 +0100

    fix and refactor async db function

--- utils/cleanup.js
+++ utils/cleanup.js
@@ -2,12 +2,39 @@ const mongojs = require('mongojs');
 const db = mongojs('grabaketak', ['users']);
 const path = require('path');
 const fs = require('fs');
+
 const { uploadFolder } = require('./config');
 
-const bannerBeginning = "------------ Audio zaharren garbiketa hasieratzen ------------";
-const bannerEnding = "------------   Audio zaharren garbiketa amaitua   ------------"
 const executionIntervalDays = 5;
 const projectRoot = path.resolve(__dirname, '..');
+
+const deleteFileAndEntry = async filename => {
+    return new Promise((resolve, reject) => {
+        db.users.remove({ "filename" : filename }, error => {
+            if (error) {
+                console.error(`Errorea datu-basetik ${filename} duen tupla ezabatzean. Error:`, error);
+            }
+            const filePath = path.join(projectRoot, uploadFolder, filename);
+            fs.unlink(filePath, error => {
+                if (error) {
+                    console.error(`Errorea sistematik ${filename} izena duen fitxategia ezabatzean. Error:`, error);
+                    reject(error);
+                }
+                else {
+                    console.log(`${filename} fitxategia ezabatu da.`);
+                    resolve();
+                }
+            });
+        });
+    });
+}
+
+const deleteOldAudioEntries = audioEntries => {
+    audioEntries.forEach(async entry => {
+        const filename = entry.filename;
+        await deleteFileAndEntry(filename);
+    });
+}
 
 const cleanup = () => {
     const cleanupTime = new Date();
@@ -15,38 +42,17 @@ const cleanup = () => {
     const cleanupTimeMs = cleanupTime.getTime();
 
     db.users.find({ "date" : { "$lt" : cleanupTimeMs }},
-        (error, userFiles) => {
-            console.log(bannerBeginning);
+        (error, audioEntries) => {
+            console.log('------------ Audio zaharren garbiketa hasieratzen ------------');
             if (error) {
                 console.error(error);
-                return console.log(bannerEnding);
             }
-            deleteOldUserEntries(userFiles);
-            console.log(bannerEnding);
+            else {
+                deleteOldAudioEntries(audioEntries);
+            }
+            console.log('------------   Audio zaharren garbiketa amaitua   ------------');
         }
     );
 };
-
-const deleteOldUserEntries = userEntries => {
-    userEntries.forEach(element => {
-        const filename = element.filename;
-        db.users.remove({ "filename" : filename },
-            error => {
-                if (error) {
-                    console.error(`Errorea datu-basetik ${filename} duen tupla ezabatzean. Error:`, error);
-                }
-                const filePath = path.join(projectRoot, uploadFolder, filename);
-                fs.unlink(filePath, error => {
-                    if (error) {
-                        console.error(`Errorea sistematik ${filename} izena duen fitxategia ezabatzean. Error:`, error);
-                    }
-                    else {
-                        console.log(`${filename} fitxategia ezabatu da.`);
-                    }
-                });
-            }
-        );
-    });
-}
 
 module.exports = cleanup;
